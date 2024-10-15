73ca404d1c9c8fa90a250d60c30d90a8705e65ef
OwlVi <mrowlvi@gmail.com>
2024-10-15 23:32:58 +0700

    Add Is_delete

--- domain/entities/fabric.go
+++ domain/entities/fabric.go
@@ -4,5 +4,6 @@ type Fabric struct {
 	Fabric_id  string `json:"fabric_id" db:"fabric_id"`
 	Fabric_url string `json:"fabric_url" db:"fabric_url"`
 	Quantity   int    `json:"quantity" db:"quantity"`
+	Is_delete  int    `json:"is_delete" db:"is_delete"`
 	Timestamp  string `json:"timestamp" db:"timestamp"`
 }
